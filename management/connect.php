ea3b6aa40d9f3026dded9d35b9f774be4751d497
Marc Raineer Filosopo <marcraineer0089@gmail.com>
2022-07-15 01:42:21 +0800

    Changes Connection from Server to Localhost

--- management/connect.php
+++ management/connect.php
@@ -3,11 +3,11 @@
 // Connection variables
 $host = "localhost"; // MySQL host name eg. localhost
 
-$user = "codemon"; // MySQL user. eg. root ( if your on localserver)
+$user = "root"; // MySQL user. eg. root ( if your on localserver)
 
-$password = "09204353341_account"; // MySQL user password (if password is not set for your root user then keep it empty )
+$password = ""; // MySQL user password (if password is not set for your root user then keep it empty )
 
-$database = "codemon_db_school_5"; // MySQL Database name
+$database = "db_school_5"; // MySQL Database name
 
 // Connect to MySQL Database
 $conn = new mysqli($host, $user, $password, $database);
