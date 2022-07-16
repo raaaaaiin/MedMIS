4d6c62a3af35975880db25daab2d9691edf62b86
Marc Raineer Filosopo <marcraineer0089@gmail.com>
2022-07-17 02:07:32 +0800

    Location php fixed

--- management/location.php
+++ management/location.php
@@ -1,106 +1,19 @@
 <?php
-// Initialize the session
 session_start();
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: process.php");
  require_once "connect.php";
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: index.php");
     exit;
 }
- 
-// Include config file
-require_once "../connect.php";
- 
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = "";
- 
-// Processing form data when form is submitted
-if(isset($_POST['signin'])){
- 
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = "";
-    } else{
-        $username = trim($_POST["username"]);
-    }
-    
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT u_id, username, password FROM user_account WHERE username =?";
-        
-        if($stmt = mysqli_prepare($conn, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = $username;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
-                            date_default_timezone_set('Asia/Manila'); 
-							$date = date('m/d/Y h:i:s a', time());
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["user_id"] = $id;
-                            $_SESSION["username"] = $username;                             
-                            
-							if (mysqli_query($conn, $sql)) {	}
-                            // Redirect user to welcome page
-                            header("location: process.php");
-                        } else{
-                            // Display an error message if password is not valid
-                            $password_err = "The password you entered was not valid.";
-                        }
-                    }
-                } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = "No account found with that username.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-        }
-        
-        // Close statement
-        mysqli_stmt_close($stmt);
-    }
-    
-    // Close connection
-    mysqli_close($conn);
-}
-if(isset($_POST['goto'])){
-	echo '
-			<script type = "text/javascript">
-					window.location.href="../login.php";
-			</script>
-				';			
-}
 ?>
 <html lang="en">
-  <title>MediGrab</title>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>MediGrab</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
@@ -156,38 +69,146 @@ if(isset($_POST['goto'])){
 						 $result1=mysqli_query($conn,$sql1);
 						 $data1=mysqli_fetch_assoc($result1);
 					     echo "<text style='font-size:15px;'>".$data1['total1']."</text>";
-
-
-
-
-
-
-
-
+					
+				    ?>
+				</h5>
+			</a>
+			<a href="cashin.php" style="float:right;margin-right:20px;text-decoration:none;">E-Coins: 
+			  <?php
+			  $qc = $conn->query("SELECT * FROM `cashin` WHERE `cashin_user_id` = '$u_id'") or die(msqli_error());
+			  $fc = $qc->fetch_array();
+			  $total_cash = $fc['cashin_total'];
+		       if($fc['cashin_total']==null){
+		          echo "0"; 
+		       }else{
+		           echo  $fc['cashin_total'];
+		       }
+			  ?>  
+			</a>  
+            <div class="container px-4 px-lg-5 mt-5">
+				 <form class="user"  method = "POST" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype = "multipart/form-data" autocomplete="off">
+                
+                    <input type="text" class="form-control"   style=" text-transform:capitalize;"required = "required"placeholder="Last Name" name = "lname" value="<?php echo $f['lname']?>" > <br>
+                 
+                    <input type="text" class="form-control"	style=" text-transform:capitalize;"required = "required"placeholder="First Name" name = "fname"  value="<?php echo $f['fname']?>"><br>
+                 
+                    <input type="text" class="form-control"	style=" text-transform:capitalize;"placeholder="Middle Name" name = "mname"  value="<?php echo $f['mname']?>"><br>
+               
+					<input type="text" class="form-control"  required = "required" name="city" placeholder="City"  value="<?php echo $f['city']?>"><br>
+				 
+						<input type="text" class="form-control "required = "required" name="brgy"  placeholder="Barangay"  value="<?php echo $f['brgy']?>"><br>
+			
+					<input type="email" class="form-control"  required = "required"name="email" id="email2" placeholder="Email Address"  value="<?php echo $f['email']?>"><br>
+					<div id="status2"></div>
+						<input type="text" class="form-control "required = "required" name="username" id="username2" placeholder="User Name"  value="<?php echo $f['username']?>"><br>
+						<div id="status"></div>
+				 
                     <input type="password" class="form-control"  placeholder="Password" name = "password" id="password2" onkeyup='check();' ><br>
+                  
+                    <input type="password" class="form-control" name = "confirm_password" id="confirm_password" placeholder="Repeat Password" onkeyup='check();'>
+					
+					<div id="err2"></div><br>
+                    <input type="password" class="form-control" required = "required" name = "old_password2" id="confirm_password" placeholder="Old Password" ><br>
+                    <input type="hidden" class="form-control"	  name = "u_id" required value="<?php echo $f['u_id']?>">
+                    <input type="hidden" class="form-control"	 name = "old_password1"  value="<?php echo $f['password']?>">
+                  
+                    <button  class = "btn btn-primary btn-user" name="check"  id="register" style="float:right;"><span class = "glyphicon glyphicon-save"></span> Update Profile</button>
+					<button  class = "btn btn-primary btn-user" name=""  id="" style="float:right;margin-right: 5px;">
+					<span class = "glyphicon glyphicon-save">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+</svg></span> Set Address
+					</button>
+					<br><br>
+				<hr>
+              </form>
              
-								
-					 
-                    <hr>
-                    
-                  </form>
-                  <div class="text-center">
-				  <a href="register.php">Sign Up Now</registration>
-                  </div>
-                </div>
-    
+			
+				<?php
+				if(isset($_POST['check'])){
+					$u_id 		= $_POST['u_id'];
+					$lname 		= $_POST['lname'];
+					$mname 		= $_POST['mname'];
+					$fname 		= $_POST['fname'];
+					$email 		= $_POST['email'];
+					$username 	= $_POST['username'];
+					$city 		= $_POST['city'];
+					$brgy 		= $_POST['brgy'];
+					$old_password1 		= $_POST['old_password1'];
+					$old_password2 		= $_POST['old_password2'];
+					$password 	    	= $_POST['password'];
+					$confirm_password 	= $_POST['confirm_password'];
+					
+					if(password_verify($old_password2, $old_password1)){
+					if($password == $confirm_password){
+						if($_POST['password']==""){
+							$ps2 = $_POST['old_password2'];
+						}else{
+							$ps2 = $_POST['password'];
+						}
+				    $pass = password_hash($ps2, PASSWORD_DEFAULT);
+					
+				    $result=$conn->query("UPDATE user_account SET lname='$lname',`brgy`='$brgy',`city`='$city',`username`='$username',fname='$fname',mname='$mname',email='$email',password='$pass' WHERE u_id='$u_id' ");
+					if($result){
+					echo '<script>
+									function myFunction() {
+										swal({
+										title: "Success!",
+										text: "Successfully Updated Your Profile",
+									    icon: "success",
+										type: "success"
+										}).then(function() {
+										window.location = "setting.php";
+									  });}
+									</script>';
+						}
+					    
+					}else{
+					    echo '<script>
+									function myFunction() {
+									swal({
+									title: "Failed!",
+									text: "Oops! Sorry, please try again",
+									icon: "error",
+									button: "Ok",
+									});}
+									</script>';
+					}
+				}else{
+				      echo '<script>
+									function myFunction() {
+									swal({
+									title: "Failed!",
+									text: "Oops! Wrong password. Please try again",
+									icon: "error",
+									button: "Ok",
+									});}
+									</script>';
+				}
+					
+				}
+					?>
             </div>
-          </div>
-        </div>
-
-      </div>
-
-    </div>
-
-
-
-
-
-</body>
-
+        </section>
+        <!-- Footer-->
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+<script>
+var check = function() {
+  if (document.getElementById('password2').value ==
+    document.getElementById('confirm_password').value) {
+    document.getElementById('password2').style.border = 'white 2px solid';
+    document.getElementById('confirm_password').style.border = 'white 2px solid';
+    document.getElementById('err2').innerHTML = '<br><span style="color:white;font-size:8px;" > </span> Password confirm';
+  } else {
+    document.getElementById('password2').style.border = 'red 2px solid';
+    document.getElementById('confirm_password').style.border = 'red 2px solid';
+    document.getElementById('err2').innerHTML = '<br><span style="color:red;font-size:8px;" > </span> Password and confirm password is not match';
+  }
+}
+</script>
+    </body>
 </html>
