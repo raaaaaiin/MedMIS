b329d471e53fe7376c200c7f5e2b2af6dcccb294
raaaaaiin <marcraineer0089@gmail.com>
2022-10-06 20:27:39 +0800

    Connection from live to local

--- management/connect.php
+++ management/connect.php
@@ -3,9 +3,9 @@ date_default_timezone_set('Asia/Manila');
 // Connection variables
 $host = "localhost"; // MySQL host name eg. localhost
 
-$user = "codemon"; // MySQL user. eg. root ( if your on localserver)
+$user = "root"; // MySQL user. eg. root ( if your on localserver)
 
-$password = "09204353341_account"; // MySQL user password (if password is not set for your root user then keep it empty )
+$password = ""; // MySQL user password (if password is not set for your root user then keep it empty )
 
 $database = "codemon_db_school_5"; // MySQL Database name
 
